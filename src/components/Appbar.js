afc0fd19bdedb0ec58c3bbf8b6c3661fa5431715
Daisuke Kozuma <you@example.com>
2020-02-25 00:20:56 +0900

    分からんすぎたいあほ

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -6,7 +6,7 @@ class Appbar extends React.Component{
     return(
       <View style={styles.appbar}>
       <View>
-      <Text style={styles.appbarTitle}>名前変更</Text>
+      <Text style={styles.appbarTitle}>名前変更したんですが</Text>
       </View>
       </View>
     );
