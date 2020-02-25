f609b36f7f64ec31b764e03fff62a78ae404d60f
Daisuke Kozuma <you@example.com>
2020-02-25 19:43:08 +0900

    test commit

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -6,7 +6,7 @@ class Appbar extends React.Component {
     return (
       <View style={styles.appbar}>
         <View>
-          <Text style={styles.appbarTitle}>TestApp</Text>
+          <Text style={styles.appbarTitle}>MemoApp</Text>
         </View>
       </View>
     );
