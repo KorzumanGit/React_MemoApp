f9e9809684b4509c0317d8b4e8586311040c4bb5
shunwitter <3123900+shunwitter@users.noreply.github.com>
2020-03-01 08:43:40 +0900

    Fix App.js

--- App.js
+++ App.js
@@ -1,51 +1,50 @@
-// 元のコード
-// import React from 'react';
-// import { StyleSheet, View } from 'react-native';
+// 必要
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 // 追加分
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-// 元のコード
+// 必要
 // import Appbar from './src/components/Appbar';
-// import SignupScreen from './src/screens/SignupScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 // 追加分
 import MemoListScreen from './src/screens/MemoListScreen';
 
 
-// 元のコード
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Appbar />
-//         <SignupScreen />
-//       </View>
-//     );
-//   }
-// }
+// 必要（ただし export default を削除）
+class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SignupScreen />
+      </View>
+    );
+  }
+}
 
-// 追加分
-createStackNavigator({
+// 変数に代入して、createAppContainerに渡します。
+const AppContainer = createStackNavigator({
   Home: { screen: MemoListScreen },
 });
 
 
 
 
-// 元のコード
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#FFFDF6',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     paddingTop: 78,
-//   },
-// });
+// 必要
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFDF6',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 78,
+  },
+});
 
 
-// 追加分
-export default createAppContainer(App);
+// こちらを export default します。
+export default createAppContainer(AppContainer);
 
